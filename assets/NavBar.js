e03a77a794a2baf6fc03e64fe0a28485b2aca546
HackOverflow404 <medhansh2005@gmail.com>
2023-11-22 16:00:23 -0600

    fixed index page navbar bug

--- assets/NavBar.js
+++ assets/NavBar.js
@@ -3,27 +3,27 @@ class NavBar extends React.Component {
         return (
             <div className="navbar">
                 <link rel="stylesheet" href="/cyberawareness/assets/NavBar.css" />
-                <div className="home-dropdown dropdown"><a id="home-link" href="../index.html"><button className="home-dropbtn dropbtn">Home</button></a></div>
+                <div className="home-dropdown dropdown"><a id="home-link" href="/cyberawareness/index.html"><button className="home-dropbtn dropbtn">Home</button></a></div>
                 <div className="passwd-dropdown dropdown">
                     <button className="passwd-dropbtn dropbtn">Passwords</button>
                     <div className="passwd-content dropdown-content">
-                        <a href="../passwords/passwdWork.html">How do Passwords Work?</a>
-                        <a href="../passwords/passwdCrack.html">How are Passwords Cracked?</a>
-                        <a href="../passwords/passwdProtect.html">How can Passwords be Protected?</a>
+                        <a href="/cyberawareness/passwords/passwdWork.html">How do Passwords Work?</a>
+                        <a href="/cyberawareness/passwords/passwdCrack.html">How are Passwords Cracked?</a>
+                        <a href="/cyberawareness/passwords/passwdProtect.html">How can Passwords be Protected?</a>
                     </div>
                 </div>
                 <div className="malware-dropdown dropdown">
                     <button className="malware-dropbtn dropbtn">Malware</button>
                     <div className="malware-content dropdown-content">
-                        <a href="../malware/malwareWhat.html">What is Malware?</a>
-                        <a href="../malware/malwareProtect.html">How can we Protect Against Malware?</a>
+                        <a href="/cyberawareness/malware/malwareWhat.html">What is Malware?</a>
+                        <a href="/cyberawareness/malware/malwareProtect.html">How can we Protect Against Malware?</a>
                     </div>
                 </div>
                 <div className="phishing-dropdown dropdown">
                     <button className="phishing-dropbtn dropbtn">Phishing</button>
                     <div className="phishing-content dropdown-content">
-                        <a href="../phishing/phishingWhat.html">What are Phishing Attacks?</a>
-                        <a href="../phishing/phishingProtect.html">How can we Protect Against Phishing Attacks?</a>
+                        <a href="/cyberawareness/phishing/phishingWhat.html">What are Phishing Attacks?</a>
+                        <a href="/cyberawareness/phishing/phishingProtect.html">How can we Protect Against Phishing Attacks?</a>
                     </div>
                 </div>
             </div>
